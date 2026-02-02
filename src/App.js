cd84adce09d98494a6298b38bb34b2a1f9493787
Minerva CYC <your-github-email@example.com>
2026-02-02 12:57:19 +0100

    Force Fix App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from './components/Login';
 import ChatRoom from './components/ChatRoom';
 import './App.css';
 
+// FORCE RESTORE V4
 function App() {
   const [user, setUser] = useState(null);
 
